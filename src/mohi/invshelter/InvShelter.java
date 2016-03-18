fcee5892a82fa49beca989548e2d083fc04080d0
MohiPE <0428asd@naver.com>
2016-03-19 00:46:01 +0900

    1.0 release

---
+++ src/mohi/invshelter/InvShelter.java
@@ -0,0 +1,32 @@
+/**
+	*		@author : MohiPE
+	*		@since : 2016.3.18
+	*		@E-Mail : dreamaker7770@gmail.com
+	*/
+package mohi.invshelter;
+
+import cn.nukkit.event.Listener;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.event.player.PlayerDeathEvent;
+
+public class InvShelter extends PluginBase implements Listener {
+	private Config config;
+	
+	@Override
+	public void onEnable() {
+		config = (new Config( getDataFolder() + "/config.yml", Config.YAML, new LinkedHashMap<String, Object>() {
+			put("Inventory save", true);
+		})).getAll();
+		getServer().getPluginManager().registerEvents( this, this );
+	}
+	@EventHandler
+	public void onDeath(PlayerDeathEvent event) {
+		if(this.config.get("Inventory save") == true) {
+			event.setKeepInventory();
+			return;
+		}
+		return;
+	}
+	
+}
