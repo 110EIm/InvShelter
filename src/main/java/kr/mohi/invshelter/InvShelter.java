5b2eea0afe18f3fa9f4b9fccab7660100e2372b7
MohiPE <0428asd@naver.com>
2016-03-20 16:00:34 +0900

    Debug and now uses maven

--- src/main/java/kr/mohi/invshelter/InvShelter.java
+++ src/main/java/kr/mohi/invshelter/InvShelter.java
@@ -3,30 +3,36 @@
 	*		@since : 2016.3.18
 	*		@E-Mail : dreamaker7770@gmail.com
 	*/
-package mohi.invshelter;
+package kr.mohi.invshelter;
 
+import java.util.LinkedHashMap;
+
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerDeathEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
-import cn.nukkit.event.player.PlayerDeathEvent;
 
 public class InvShelter extends PluginBase implements Listener {
 	private LinkedHashMap<String, Object> config;
-	
+
 	@Override
 	public void onEnable() {
 		config = (LinkedHashMap<String, Object>) (new Config( getDataFolder() + "/config.yml", Config.YAML, new LinkedHashMap<String, Object>() {
-			put("Inventory save", true);
+			{
+				put("Inventory save", true);
+			}
 		})).getAll();
 		getServer().getPluginManager().registerEvents( this, this );
 	}
+
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event) {
-		if(this.config.get("Inventory save") == true) {
-			event.setKeepInventory();
+		if ((Boolean) this.config.get("Inventory save") == true) {
+			event.setKeepInventory(true);
 			return;
 		}
 		return;
 	}
-	
+
 }
