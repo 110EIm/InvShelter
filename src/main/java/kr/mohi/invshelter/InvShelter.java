ad0fd2d1e75635a6ff943af1f41237947525ac51
MohiPE <0428asd@naver.com>
2016-03-24 17:11:27 +0900

    Release

--- src/main/java/kr/mohi/invshelter/InvShelter.java
+++ src/main/java/kr/mohi/invshelter/InvShelter.java
@@ -11,14 +11,18 @@ import java.util.LinkedHashMap;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerDeathEvent;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
+import cn.nukkit.event.TextContainer;
 
 public class InvShelter extends PluginBase implements Listener {
 	private LinkedHashMap<String, Object> config;
 
 	@Override
 	public void onEnable() {
+		getDataFolder().mkdir();
 		config = (LinkedHashMap<String, Object>) (new Config( getDataFolder() + "/config.yml", Config.YAML, new LinkedHashMap<String, Object>() {
 			{
 				put("Inventory save", true);
@@ -31,9 +35,19 @@ public class InvShelter extends PluginBase implements Listener {
 	public void onDeath(PlayerDeathEvent event) {
 		if ((Boolean) this.config.get("Inventory save") == true) {
 			event.setKeepInventory(true);
-			return;
 		}
-		return;
+		event.setDeathMessage(new TextContainer(""));
 	}
-
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if(! event.getPlayer().isOp())
+			event.setJoinMessage(new TextContainer(""));
+		//else event.setJoinMessage( "관리자 " + event.getPlayer().getName() + "가 접속하였습니다." );
+	}
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		if(! event.getPlayer().isOp())
+			event.setQuitMessage(new TextContainer(""));
+		//else event.setQuitMessage( "관리자 " + event.getPlayer().getName() + "가 접속하였습니다." );
+	}
 }
